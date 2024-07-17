774e0301ca904dd2979c3c8c859a3ec6ed0b367b
ahmed <ahmed-abotaleb@outlook.com>
2024-07-17 04:21:53 +0300

    init empty project with essential plugins and configs

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div >
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+
+
+body{
+  min-height: 100dvh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="flex items-center justify-center h-screen text-9xl">
+    <h1>Hello world</h1>
+  </main>
+</template>
